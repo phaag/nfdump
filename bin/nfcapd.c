3e55add0613ce0092a0ce835095d57dc55588f5e
Peter Haag <peter@people.ops-trust.net>
2018-11-25 12:53:26 +0100

    Add missing -M command line help to nfcapd

--- bin/nfcapd.c
+++ bin/nfcapd.c
@@ -1,8 +1,5 @@
 /*
- *  Copyright (c) 2017, Peter Haag
- *  Copyright (c) 2016, Peter Haag
- *  Copyright (c) 2014, Peter Haag
- *  Copyright (c) 2009, Peter Haag
+ *  Copyright (c) 2009-2017, Peter Haag
  *  Copyright (c) 2004-2008, SWITCH - Teleinformatikdienste fuer Lehre und Forschung
  *  All rights reserved.
  *  
