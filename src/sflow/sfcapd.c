d5d6d7a0aba9ab873dfc456df06d5a1f701b3f73
Arkadiusz <arkadiusz.piorkowski@nask.pl>
2021-06-29 13:08:20 +0200

    keep code symiliarity (as max as possible) between nfcapd and sfcapd

--- src/sflow/sfcapd.c
+++ src/sflow/sfcapd.c
@@ -34,32 +34,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 #include <stdint.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <time.h>
+#include <sys/time.h>
 #include <netdb.h>
 #include <pwd.h>
 #include <grp.h>
-#include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/param.h>
-#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <time.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <sys/mman.h>
 #include <string.h>
-#include <libgen.h>
 #include <errno.h>
 #include <dirent.h>
 
 #ifdef PCAP
 #include "pcap_reader.h"
 #endif
-
 
 #include "util.h"
 #include "nfdump.h"
@@ -85,12 +83,11 @@
 #endif
 
 #include "expire.h"
-
 #include "sflow_nfdump.h"
 
 #define DEFAULTSFLOWPORT "6343"
 
-static void *shmem;
+static void *shmem = NULL;
 static int verbose = 0;
 
 // Define a generic type to get data from socket or pcap file
@@ -102,6 +99,7 @@ static FlowSource_t *FlowSource;
 static int done, launcher_alive, periodic_trigger, launcher_pid;
 
 static const char *nfdump_version = VERSION;
+
 
 /* Local function Prototypes */
 static void usage(char *name);
@@ -116,27 +114,26 @@ static void daemonize(void);
 
 static void SetPriv(char *userid, char *groupid );
 
-static void run(packet_function_t receive_packet, int socket, repeater_t *repeater,
+static void run(packet_function_t receive_packet, int socket, repeater_t *repeater, 
 	time_t twin, time_t t_begin, int report_seq, int use_subdirs, char *time_extension, int compress);
 
 /* Functions */
 static void usage(char *name) {
 		printf("usage %s [options] \n"
 					"-h\t\tthis text you see right here\n"
-					"-u userid\tChange user to userid\n"
-					"-g groupid\tChange group to groupid\n"
+					"-u userid\tChange user to username\n"
+					"-g groupid\tChange group to groupname\n"
 					"-t interval\tset the interval to rotate sfcapd files\n"
 					"-b host\t\tbind socket to host/IP addr\n"
 					"-J mcastgroup\tJoin multicast group <mcastgroup>\n"
 					"-p portnum\tlisten on port portnum\n"
-					"-l logdir \tset the output directory. (no default) \n"
+					"-l basdir \tset the output directory. (no default) \n"
 					"-S subdir\tSub directory format. see nfcapd(1) for format\n"
 					"-I Ident\tset the ident string for stat file. (default 'none')\n"
-					"-H Add port histogram data to flow file.(default 'no')\n"
 					"-n Ident,IP,logdir\tAdd this flow source - multiple streams\n" 
 					"-N sourceFile\tAdd flows from sourceFile\n"
 					"-P pidfile\tset the PID file\n"
-					"-R IP[/port]\tRepeat incoming packets to IP address/port. Max 8 repeaters\n"
+					"-R IP[/port]\tRepeat incoming packets to IP address/port. Max 8 repeaters.\n"
 					"-x process\tlaunch process after a new file becomes available\n"
 					"-z\t\tLZO compress flows in output file.\n"
 					"-y\t\tLZ4 compress flows in output file.\n"
@@ -144,7 +141,7 @@ static void usage(char *name) {
 					"-B bufflen\tSet socket buffer to bufflen bytes\n"
 					"-e\t\tExpire data at each cycle.\n"
 					"-D\t\tFork to background\n"
-					"-E\t\tPrint extended format of sflow data. for debugging purpose only.\n"
+					"-E\t\tPrint extended format of sflow data. For debugging purpose only.\n"
 					"-4\t\tListen on IPv4 (default).\n"
 					"-6\t\tListen on IPv6.\n"
 					"-V\t\tPrint version and exit.\n"
@@ -212,6 +209,7 @@ static void IntHandler(int signal) {
 
 } /* End of IntHandler */
 
+
 static void daemonize(void) {
 int fd;
 	switch (fork()) {
@@ -221,16 +219,16 @@ int fd;
 		case -1:
 			// error
 			LogError("fork() error: %s", strerror(errno));
-			exit(EXIT_FAILURE);
+			exit(EXIT_SUCCESS);
 			break;
 		default:
 			// parent
-			_exit(EXIT_FAILURE);
+			_exit(EXIT_SUCCESS);
 	}
 
 	if (setsid() < 0) {
 		LogError("setsid() error: %s", strerror(errno));
-		exit(EXIT_FAILURE);
+		exit(EXIT_SUCCESS);
 	}
 
 	// Double fork
@@ -241,10 +239,11 @@ int fd;
 		case -1:
 			// error
 			LogError("fork() error: %s", strerror(errno));
-			exit(EXIT_FAILURE);
+			exit(EXIT_SUCCESS);
 			break;
 		default:
-			_exit(EXIT_FAILURE);
+			// parent
+			_exit(EXIT_SUCCESS);
 	}
 
 	fd = open("/dev/null", O_RDONLY);
@@ -286,7 +285,7 @@ int		err;
 		newuid = pw_entry ? pw_entry->pw_uid : atol(userid);
 
 		if ( newuid == 0 ) {
-			LogError("Invalid user '%s", userid);
+			LogError("Invalid user '%s'", userid);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -321,7 +320,7 @@ int		err;
 #include "nffile_inline.c"
 #include "collector_inline.c"
 
-static void run(packet_function_t receive_packet, int socket, repeater_t *repeater,
+static void run(packet_function_t receive_packet, int socket, repeater_t *repeater, 
 	time_t twin, time_t t_begin, int report_seq, int use_subdirs, char *time_extension, int compress) {
 FlowSource_t			*fs;
 struct sockaddr_storage sf_sender;
@@ -337,14 +336,14 @@ srecord_t	*commbuff;
 
 	in_buff  = malloc(NETWORK_INPUT_BUFF_SIZE);
 	if ( !in_buff ) {
-		LogError("malloc() buffer allocation error: %s", strerror(errno));
+		LogError("malloc() allocation error in %s line %d: %s", __FILE__, __LINE__, strerror(errno) );
 		return;
 	}
 
 	// init vars
 	commbuff = (srecord_t *)shmem;
 
-	// Init each netflow source output data buffer
+	// Init each sflow source output data buffer
 	fs = FlowSource;
 	while ( fs ) {
 
@@ -355,8 +354,8 @@ srecord_t	*commbuff;
 		}
 		SetIdent(fs->nffile, fs->Ident);
 
-		// init stat vars
-		fs->bad_packets		= 0;
+		// init vars
+		fs->bad_packets = 0;
 		fs->msecFirst	= 0xffffffffffffLL;
 		fs->msecLast	= 0;
 
@@ -368,6 +367,7 @@ srecord_t	*commbuff;
 	t_start = t_begin;
 
 	cnt = 0;
+	periodic_trigger = 0;
 	ignored_packets  = 0;
 
 	// wake up at least at next time slot (twin) + 1s
@@ -384,20 +384,19 @@ srecord_t	*commbuff;
 
 		/* read next bunch of data into beginn of input buffer */
 		if ( !done) {
-
 #ifdef PCAP
-
-			// Debug code to read from pcap file
-			// cnt = NextPacket(in_buff, NETWORK_INPUT_BUFF_SIZE);
-			cnt = receive_packet (socket, in_buff, NETWORK_INPUT_BUFF_SIZE , 0, 
-				(struct sockaddr *)&sf_sender, &sf_sender_size);
-			if ( cnt == -2 )
+			// Debug code to read from pcap file, or from socket 
+			cnt = receive_packet(socket, in_buff, NETWORK_INPUT_BUFF_SIZE , 0, 
+						(struct sockaddr *)&sf_sender, &sf_sender_size);
+						
+			// in case of reading from file EOF => -2
+			if ( cnt == -2 ) 
 				done = 1;
 #else
-
 			cnt = recvfrom (socket, in_buff, NETWORK_INPUT_BUFF_SIZE , 0, 
-				(struct sockaddr *)&sf_sender, &sf_sender_size);
+						(struct sockaddr *)&sf_sender, &sf_sender_size);
 #endif
+
 			if ( cnt == -1 && errno != EINTR ) {
 				LogError("ERROR: recvfrom: %s", strerror(errno));
 				continue;
@@ -416,6 +415,7 @@ srecord_t	*commbuff;
 		}
 
 		/* Periodic file renaming, if time limit reached or if we are done.  */
+		// t_now = time(NULL);
 		gettimeofday(&tv, NULL);
 		t_now = tv.tv_sec;
 
@@ -436,7 +436,7 @@ srecord_t	*commbuff;
 			
 					// failed to generate subdir path - put flows into base directory
 					subdir = NULL;
-				}
+				} 
 			} else {
 				subdir = NULL;
 			}
@@ -461,8 +461,10 @@ srecord_t	*commbuff;
 					snprintf(nfcapd_filename, MAXPATHLEN-1, "%s/nfcapd.%s", fs->datadir, fmt);
 				}
 				nfcapd_filename[MAXPATHLEN-1] = '\0';
-
+	
 				// update stat record
+				// if no flows were collected, fs->msecLast is still 0
+				// set first_seen to start of this time slot, with twin window size.
 				if ( fs->msecLast == 0 ) {
 					fs->msecFirst = 1000LL * (uint64_t)t_start;
 					fs->msecLast  = 1000LL * (uint64_t)(t_start + twin);
@@ -472,12 +474,13 @@ srecord_t	*commbuff;
 
 				// Flush Exporter Stat to file
 				FlushExporterStats(fs);
-				// Write Stat record and close file
+				// Close file
 				CloseUpdateFile(nffile);
 
+				// if rename fails, we are in big trouble, as we need to get rid of the old .current file
+				// otherwise, we will loose flows and can not continue collecting new flows
 				if ( rename(fs->current, nfcapd_filename) < 0 ) {
 					LogError("Ident: %s, Can't rename dump file: %s", fs->Ident,  strerror(errno));
-					LogError("Ident: %s, Serious Problem! Fix manually", fs->Ident);
 					if ( launcher_pid )
 						commbuff->failed = 1;
 
@@ -495,15 +498,14 @@ srecord_t	*commbuff;
 
 				// log stats
 				LogInfo("Ident: '%s' Flows: %llu, Packets: %llu, Bytes: %llu, Sequence Errors: %u, Bad Packets: %u", 
-					fs->Ident, (unsigned long long)nffile->stat_record->numflows,
+					fs->Ident, (unsigned long long)nffile->stat_record->numflows, 
 					(unsigned long long)nffile->stat_record->numpackets, 
 					(unsigned long long)nffile->stat_record->numbytes, nffile->stat_record->sequence_failure, fs->bad_packets);
 
-				// reset stat record
+				// reset stats
 				fs->bad_packets = 0;
-				fs->msecFirst	= 0xffffffffffffLL;
-				fs->msecLast	= 0;
-
+				fs->msecFirst   = 0xffffffffffffLL;
+				fs->msecLast 	= 0;
 
 				if ( !done ) {
 					fs->nffile = OpenNewFile(fs->current, fs->nffile, compress, NOT_ENCRYPTED);
@@ -512,18 +514,19 @@ srecord_t	*commbuff;
 						break;
 					}
 
-					// Dump all extension maps to the buffer
+					// Dump all exporters/samplers to the buffer
 					FlushStdRecords(fs);
 				}
 
 				// next flow source
 				fs = fs->next;
+
 			} // end of while (fs)
 
-			// All flow sources updated - signal launcher if required
+			// trigger launcher if required
 			if ( launcher_pid ) {
 				// Signal launcher
-		
+
 				strncpy(commbuff->tstring, fmt, MAXTIMESTRING);
 				commbuff->tstring[MAXTIMESTRING-1] = '\0';
 
@@ -553,10 +556,11 @@ srecord_t	*commbuff;
 			t_start += twin;
 			/* t_start = filename time stamp: begin of slot
 		 	* + twin = end of next time interval
-		 	* + 1 = if no data is collected, this is at latest to act
+		 	* + 1 = act at least 1s after time window expired
 		 	* - t_now = difference value to now
 		 	*/
 			alarm(t_start + twin + 1 - t_now);
+
 		}
 
 		/* check for error condition or done . errno may only be EINTR */
@@ -581,7 +585,6 @@ srecord_t	*commbuff;
 			continue;
 
 		// get flow source record for current packet, identified by sender IP address
-
 		fs = GetFlowSource(&sf_sender);
 		if ( fs == NULL ) {
 			LogError("Skip UDP packet. Ignored packets so far %u packets", ignored_packets);
@@ -589,21 +592,19 @@ srecord_t	*commbuff;
 			continue;
 		}
 
-
 		/* check for too little data - cnt must be > 0 at this point */
 		if ( cnt < sizeof(common_flow_header_t) ) {
 			LogError("Ident: %s, Data length error: too little data for common netflow header. cnt: %i",fs->Ident, (int)cnt);
 			fs->bad_packets++;
 			continue;
 		}
-		fs->received = tv;
 
+		fs->received = tv;
 		/* Process data - have a look at the common header */
 		Process_sflow(in_buff, cnt, fs);
 
 		// each Process_xx function has to process the entire input buffer, therefore it's empty now.
 		export_packets++;
-
 	}
 
 	if ( verbose && blast_failures ) {
@@ -665,7 +666,7 @@ char	*pcap_file = NULL;
 	Ident			= "none";
 	FlowSource		= NULL;
 
-	while ((c = getopt(argc, argv, "46ehEVI:DB:b:f:jl:n:N:p:J:P:R:S:T:t:x:ru:g:yzZ")) != EOF) {
+	while ((c = getopt(argc, argv, "46ef:hEVI:DB:b:jl:J:n:N:p:P:R:S:T:t:x:ru:g:yzZ")) != EOF) {
 		switch (c) {
 			case 'h':
 				usage(argv[0]);
@@ -680,9 +681,6 @@ char	*pcap_file = NULL;
 			case 'e':
 				expire = 1;
 				break;
-			case 'E':
-				verbose = 1;
-				break;
 			case 'f': {
 #ifdef PCAP
 				struct stat	fstat;
@@ -693,9 +691,12 @@ char	*pcap_file = NULL;
 					exit(254);
 				}
 #else
-				LogError("PCAP reader not compiled! Option ignored");
+				LogError("PCAP reader not compiled! Option ignored!");
 #endif
 				} break;
+			case 'E':
+				verbose = 1;
+				break;
 			case 'V':
 				printf("%s: Version: %s\n",argv[0], nfdump_version);
 				exit(EXIT_SUCCESS);
@@ -711,29 +712,8 @@ char	*pcap_file = NULL;
 					exit(EXIT_FAILURE);
 				break;
 			case 'N':
-				if ( AddFlowSourceFromFile(&FlowSource, optarg) )
+				if ( AddFlowSourceFromFile(&FlowSource, optarg) ) 
 					exit(EXIT_FAILURE);
-				break;
-			case 'j':
-				if ( compress ) {
-					LogError("Use one compression: -z for LZO, -j for BZ2 or -y for LZ4 compression");
-					exit(EXIT_FAILURE);
-				}
-				compress = BZ2_COMPRESSED;
-				break;
-			case 'y':
-				if ( compress ) {
-					LogError("Use one compression: -z for LZO, -j for BZ2 or -y for LZ4 compression");
-					exit(EXIT_FAILURE);
-				}
-				compress = LZ4_COMPRESSED;
-				break;
-			case 'z':
-				if ( compress ) {
-					LogError("Use one compression: -z for LZO, -j for BZ2 or -y for LZ4 compression");
-					exit(EXIT_FAILURE);
-				}
-				compress = LZO_COMPRESSED;
 				break;
 			case 'B':
 				bufflen = strtol(optarg, &checkptr, 10);
@@ -750,7 +730,7 @@ char	*pcap_file = NULL;
 			case 'p':
 				listenport = optarg;
 				break;
-			case 'P': 
+			case 'P':
 				pidfile = verify_pid(optarg);
 				if (!pidfile) {
 					exit(EXIT_FAILURE);
@@ -808,6 +788,27 @@ char	*pcap_file = NULL;
 			case 'x':
 				launch_process = optarg;
 				break;
+			case 'j':
+				if ( compress ) {
+					LogError("Use one compression: -z for LZO, -j for BZ2 or -y for LZ4 compression");
+					exit(EXIT_FAILURE);
+				}
+				compress = BZ2_COMPRESSED;
+				break;
+			case 'y':
+				if ( compress ) {
+					LogError("Use one compression: -z for LZO, -j for BZ2 or -y for LZ4 compression");
+					exit(EXIT_FAILURE);
+				}
+				compress = LZ4_COMPRESSED;
+				break;
+			case 'z':
+				if ( compress ) {
+					LogError("Use one compression: -z for LZO, -j for BZ2 or -y for LZ4 compression");
+					exit(EXIT_FAILURE);
+				}
+				compress = LZO_COMPRESSED;
+				break;
 			case 'Z':
 				time_extension	= "%Y%m%d%H%M%z";
 				spec_time_extension = 1;
@@ -816,7 +817,7 @@ char	*pcap_file = NULL;
 				if ( family == AF_UNSPEC )
 					family = AF_INET;
 				else {
-					LogError("ERROR, Accepts only one protocol IPv4 or IPv6");
+					LogError("ERROR, Accepts only one protocol IPv4 or IPv6!");
 					exit(EXIT_FAILURE);
 				}
 				break;
@@ -824,7 +825,7 @@ char	*pcap_file = NULL;
 				if ( family == AF_UNSPEC )
 					family = AF_INET6;
 				else {
-					LogError("ERROR, Accepts only one protocol IPv4 or IPv6");
+					LogError("ERROR, Accepts only one protocol IPv4 or IPv6!");
 					exit(EXIT_FAILURE);
 				}
 				break;
@@ -833,27 +834,23 @@ char	*pcap_file = NULL;
 				exit(EXIT_FAILURE);
 		}
 	}
-
+	
 	if (argc - optind > 1) {
 		usage(argv[0]);
 		exit(EXIT_FAILURE);
 	} 
 
-	if ( expire && spec_time_extension ) {
-		LogError("ERROR, -Z timezone extension breaks expire -e");
-		exit(EXIT_FAILURE);
-	}
-
 	if ( FlowSource == NULL && datadir == NULL ) {
 		LogError("ERROR, Missing -n (-l/-I) source definitions");
 		exit(EXIT_FAILURE);
 	}
-
-	if ( FlowSource == NULL && !AddDefaultFlowSource(&FlowSource, Ident, datadir) )
+	if ( FlowSource == NULL && !AddDefaultFlowSource(&FlowSource, Ident, datadir) ) {
+		LogError("Failed to add default data collector directory");
 		exit(EXIT_FAILURE);
+	}
 
 	if ( bindhost && mcastgroup ) {
-		LogError("ERROR, -b and -j are mutually exclusive");
+		LogError("ERROR, -b and -j are mutually exclusive!!");
 		exit(EXIT_FAILURE);
 	}
 
@@ -864,11 +861,17 @@ char	*pcap_file = NULL;
 	if ( !Init_nffile(NULL) )
 		exit(254);
 
-#ifdef PCAP
+
+	if ( expire && spec_time_extension ) {
+		LogError("ERROR, -Z timezone extension breaks expire -e");
+		exit(EXIT_FAILURE);
+	}
+
 	// Debug code to read from pcap file
+#ifdef PCAP
 	sock = 0;
 	if ( pcap_file ) {
-		printf("Setup pcap reader");
+		printf("Setup pcap reader\n");
 		setup_packethandler(pcap_file, NULL);
 		receive_packet 	= NextPacket;
 	} else 
@@ -915,14 +918,11 @@ char	*pcap_file = NULL;
 
 	done = 0;
 	if ( launch_process || expire ) {
-		// for efficiency reason, the process collecting the data
-		// and the process launching processes, when a new file becomes
-		// available are separated. Communication is done using signals
-		// as well as shared memory
-		// prepare shared memory
+
+		// create laucher comm memory struct
 		shmem = mmap(0, sizeof(srecord_t), PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0);
-		if ( shmem == MAP_FAILED ) {
-			LogError("mmap() error: %s", strerror(errno));
+		if ( shmem == MAP_FAILED) {
+			LogError("mmap() error in %s:%i: %s", __FILE__, __LINE__ , strerror(errno));
 			close(sock);
 			exit(EXIT_FAILURE);
 		}
@@ -933,7 +933,7 @@ char	*pcap_file = NULL;
 				// child
 				close(sock);
 				launcher(shmem, FlowSource, launch_process, expire);
-				exit(EXIT_SUCCESS);
+				_exit(EXIT_SUCCESS);
 				break;
 			case -1:
 				LogError("fork() error: %s", strerror(errno));
@@ -951,7 +951,7 @@ char	*pcap_file = NULL;
 	fs = FlowSource;
 	while ( fs ) {
 		if ( InitBookkeeper(&fs->bookkeeper, fs->datadir, getpid(), launcher_pid) != BOOKKEEPER_OK ) {
-			LogError("initialize bookkeeper failed.");
+			LogError("initialize bookkeeper failed");
 
 			// release all already allocated bookkeepers
 			fs = FlowSource;
@@ -1002,11 +1002,10 @@ char	*pcap_file = NULL;
 	}
 
 	LogInfo("Terminating sfcapd.");
-	EndLog();
-
 	if ( pidfile )
 		remove_pid(pidfile);
 
+	EndLog();
 	return 0;
 
 } /* End of main */
