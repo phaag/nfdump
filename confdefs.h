eded3e2aaceb6121e1c46be2b5120fbe040424af
borjam <borjam@gmail.com>
2023-02-02 09:54:45 +0100

    Added source address specification for nfreplay (flag -S)

---
+++ confdefs.h
@@ -0,0 +1,11 @@
+/* confdefs.h */
+#define PACKAGE_NAME "nfdump"
+#define PACKAGE_TARNAME "nfdump"
+#define PACKAGE_VERSION "1.7.0"
+#define PACKAGE_STRING "nfdump 1.7.0"
+#define PACKAGE_BUGREPORT "peter@people.ops-trust.net"
+#define PACKAGE_URL ""
+#define PACKAGE "nfdump"
+#define VERSION "1.7.0"
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
