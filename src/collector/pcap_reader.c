6f16426b7e097e5b7553f0b90c8b7542a209eb50
Peter Haag <peter@people.ops-trust.net>
2023-02-18 19:10:09 +0100

    Pimp pcap_reader

--- src/collector/pcap_reader.c
+++ src/collector/pcap_reader.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2009-2022, Peter Haag
+ *  Copyright (c) 2009-2023, Peter Haag
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -28,15 +28,16 @@
  *
  */
 
-#include <config.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include "config.h"
+#ifdef HAVE_NET_ETHERNET_H
+#include <net/ethernet.h>
+#endif
+#ifdef HAVE_NET_ETHERTYPES_H
+#include <net/ethertypes.h>
+#endif
+
 #define __FAVOR_BSD 1
+
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netinet/in_systm.h>
@@ -45,19 +46,27 @@
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <pcap.h>
+#include <signal.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "pcap_reader.h"
-
-#define NUMLISTS 5
-#define HASHSIZE 8192
-#define HASH_KEYLENGTH 4
-
-enum { SRC_IP, DST_IP, UDP_PORT, TCP_PORT, ICMP_TYPE };
+#include "util.h"
 
 static pcap_t *pcap_handle;
-static uint32_t udp_count, arp_count, unknow_count;
+static int linktype = 0;
+static int linkoffset = 0;
+
+typedef struct vlan_hdr_s {
+    uint16_t vlan_id;
+    uint16_t type;
+} vlan_hdr_t;
 
 /*
  * Function prototypes
@@ -103,105 +112,173 @@ static pcap_t *setup_pcap(char *fname, char *filter, char *errbuf) {
      *  We need to make sure this is Ethernet.  The DLTEN10MB specifies
      *  standard 10MB and higher Ethernet.
      */
-    if (pcap_datalink(pcap_handle) != DLT_EN10MB) {
-        snprintf(errbuf, PCAP_ERRBUF_SIZE - 1, "Snooping not on an ethernet.\n");
-        pcap_close(pcap_handle);
-        return NULL;
+    linktype = pcap_datalink(pcap_handle);
+    switch (linktype) {
+        case DLT_RAW:
+            linkoffset = 0;
+            break;
+        case DLT_PPP:
+            linkoffset = 2;
+            break;
+        case DLT_PPP_SERIAL:
+            linkoffset = 4;
+            break;
+        case DLT_NULL:
+            linkoffset = 4;
+            break;
+        case DLT_LOOP:
+            linkoffset = 14;
+            break;
+        case DLT_EN10MB:
+            linkoffset = 14;
+            break;
+        case DLT_LINUX_SLL:
+            linkoffset = 16;
+            break;
+        case DLT_IEEE802_11:
+            linkoffset = 22;
+            break;
+        case DLT_NFLOG:
+            linkoffset = 0;
+            break;
+        case DLT_PFLOG:
+            linkoffset = 0;
+            break;
+        default:
+            snprintf(errbuf, PCAP_ERRBUF_SIZE - 1, "Snooping not on an ethernet.\n");
+            pcap_close(pcap_handle);
+            return NULL;
     }
     return pcap_handle;
 
 } /* setup_pcap */
 
-static ssize_t decode_packet(struct pcap_pkthdr *hdr, u_char *pkt, void *buffer, struct sockaddr *sock) {
-    struct ip *ip;
-    struct udphdr *udp;
-    u_char *payload;
-    int len;
-    u_int hlen, version;
-    u_int packet_len;
+static ssize_t decode_packet(struct pcap_pkthdr *hdr, u_char *data, void *buffer, struct sockaddr *sock) {
     struct sockaddr_in *in_sock = (struct sockaddr_in *)sock;
+    static unsigned pkg_cnt = 0;
+
+    pkg_cnt++;
+
+    // snaplen is minimum 54 bytes
+    void *dataptr = (void *)data + linkoffset;  // after link header
+    void *eodata = (void *)data + hdr->caplen;
 
     u_int length = hdr->len;
     if (hdr->len > hdr->caplen) {
         printf("Short packet - missing: %u bytes\n", hdr->len - hdr->caplen);
-        length = hdr->caplen;
         return 0;
     }
-    ip = NULL;
 
-/*
- *  Figure out which layer 2 protocol the frame belongs to and call
- *  the corresponding decoding module.  The protocol field of an
- *  Ethernet II header is the 13th + 14th byte.  This is an endian
- *  independent way of extracting a big endian short from memory.  We
- *  extract the first byte and make it the big byte and then extract
- *  the next byte and make it the small byte.
- */
+    uint16_t protocol = 0;
+
 REDO_LINK:
-    switch (pkt[12] << 0x08 | pkt[13]) {
-        case 0x0800:
+    // link layer processing
+    switch (linktype) {
+        case DLT_EN10MB:
+            protocol = data[12] << 0x08 | data[13];
+            int IEEE802 = protocol <= 1500;
+            if (IEEE802) {
+                return 0;
+            }
+            break;
+        case DLT_RAW:
+            protocol = 0x800;
+            break;
+        case DLT_PPP:
+            protocol = 0x800;
+            break;
+        case DLT_PPP_SERIAL:
+            protocol = 0x800;
+            break;
+        case DLT_LOOP:
+        case DLT_NULL: {
+            uint32_t header;
+            if (linktype == DLT_LOOP)
+                header = ntohl(*((uint32_t *)data));
+            else
+                header = *((uint32_t *)data);
+            switch (header) {
+                case 2:
+                    protocol = 0x800;
+                    break;
+                case 24:
+                case 28:
+                case 30:
+                    protocol = 0x86DD;
+                    break;
+                default:
+                    LogInfo("Packet: %u: unsupported DLT_NULL protocol: 0x%x, packet: %u", pkg_cnt, header);
+                    return 0;
+            }
+        } break;
+        case DLT_LINUX_SLL:
+            protocol = data[14] << 8 | data[15];
+            break;
+        case DLT_IEEE802_11:
+            protocol = 0x800;
+            break;
+        default:
+            LogInfo("Packet: %u: unsupported link type: 0x%x, packet: %u", pkg_cnt, linktype);
+            return 0;
+    }
+
+    struct ip *ip = NULL;
+    switch (protocol) {
+        case ETHERTYPE_IP:
             /* IPv4 */
-            length -= 14;
-            ip = (struct ip *)&pkt[14];
+            ip = (struct ip *)dataptr;  // offset points to end of link layer
+            length -= linkoffset;
             in_sock->sin_family = AF_INET;
             in_sock->sin_addr = ip->ip_src;
 #ifdef HAVE_STRUCT_SOCKADDR_SA_LEN
             in_sock->sin_len = sizeof(struct sockaddr_in);
 #endif
             break;
-        case 0x0806:
-            /* ARP */
-            arp_count++;
-            break;
-        case 0x8100:   // VLAN
-            pkt += 4;  // strip off vlan
-            printf("Skip VLAN labels\n");
+        case ETHERTYPE_VLAN:  // VLAN
+            do {
+                vlan_hdr_t *vlan_hdr = (vlan_hdr_t *)dataptr;
+                protocol = ntohs(vlan_hdr->type);
+                dataptr += 4;
+            } while ((dataptr < eodata) && protocol == 0x8100);
+
             goto REDO_LINK;
             break;
         default:
             /* We're not bothering with 802.3 or anything else */
-            printf("PCAP unknown linktype %u%u\n", pkt[12], pkt[13]);
-            unknow_count++;
+            printf("PCAP unknown protocol %u\n", protocol);
             break;
     }
 
     /* for the moment we handle only IPv4 */
     if (!ip || ip->ip_v != 4) return 0;
 
-    len = ntohs(ip->ip_len);
-    hlen = ip->ip_hl;   /* header length */
-    version = ip->ip_v; /* ip version */
-
-    /* check version */
-    if (version != 4) {
-        fprintf(stdout, "Unknown version %d\n", version);
-        return 0;
-    }
+    u_short len = ntohs(ip->ip_len);
+    u_int hlen = ip->ip_hl; /* header length */
+    // u_int version = ip->ip_v; /* ip version */
 
     /* check header length */
     if (hlen < 5) {
-        fprintf(stdout, "bad-hlen %d \n", hlen);
+        LogError("bad-hlen %d", hlen);
         return 0;
     }
 
     /* see if we have as much packet as we should */
     if (length < len) {
-        printf("\ntruncated IP - %d bytes missing\n", len - length);
+        LogError("\ntruncated IP - %d bytes missing", len - length);
         return 0;
     }
 
     switch (ip->ip_p) {
-        case IPPROTO_UDP:
-            udp_count++;
-            udp = (struct udphdr *)((void *)ip + (ip->ip_hl << 0x02));
-            packet_len = ntohs(udp->uh_ulen) - 8;
-            payload = (u_char *)((void *)udp + sizeof(struct udphdr));
+        case IPPROTO_UDP: {
+            struct udphdr *udp = (struct udphdr *)((void *)ip + (ip->ip_hl << 0x02));
+            unsigned int packet_len = ntohs(udp->uh_ulen) - 8;
+            void *payload = (void *)((void *)udp + sizeof(struct udphdr));
 
             memcpy(buffer, payload, packet_len);
             in_sock->sin_port = udp->uh_sport;
             return packet_len;
             // unreached
-            break;
+        } break;
         default:
             /* no default */
             break;
@@ -216,7 +293,7 @@ void setup_packethandler(char *fname, char *filter) {
 
     pcap_handle = setup_pcap(fname, filter, errbuf);
     if (!pcap_handle) {
-        fprintf(stderr, "Can't init pcap: %s\n", errbuf);
+        LogError("Can't init pcap: %s", errbuf);
         exit(255);
     }
 
@@ -234,11 +311,3 @@ ssize_t NextPacket(int fill1, void *buffer, size_t buffer_size, int fill2, struc
     *size = sizeof(struct sockaddr_in);
     return decode_packet(header, pkt_data, buffer, sock);
 }
-
-/*
-int main( int argc, char **argv ) {
-
-        setup_packethandler("sflow.pack", NULL);
-        return 0;
-}
-*/
