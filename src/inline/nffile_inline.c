phaag/nfdump

commit 222419364892155de294792a262cca5e27d41993

Fix Event display time, if not flagged as event

src/inline/nffile_inline.c
@@ -241,6 +241,8 @@ static inline void ExpandRecord_v3(recordHeaderV3_t *v3Record, master_record_t *
                 output_record->event = nselCommon->fwEvent;
                 output_record->fwXevent = nselCommon->fwXevent;
                 output_record->msecEvent = nselCommon->msecEvent;
+                if (output_record->msecFirst == 0) output_record->msecFirst = nselCommon->msecEvent;
+                if (output_record->msecLast == 0) output_record->msecFirst = nselCommon->msecEvent;
             } break;
             case EXnselXlateIPv4ID: {
                 EXnselXlateIPv4_t *nselXlateIPv4 = (EXnselXlateIPv4_t *)((void *)elementHeader + sizeof(elementHeader_t));
@@ -292,6 +294,8 @@ static inline void ExpandRecord_v3(recordHeaderV3_t *v3Record, master_record_t *
                     output_record->event = nelCommon->natEvent;
                     output_record->event_flag = NAT_EVENT;
                 }
+                if (output_record->msecFirst == 0) output_record->msecFirst = output_record->msecEvent;
+                if (output_record->msecLast == 0) output_record->msecLast = output_record->msecEvent;
             } break;
             case EXnelXlatePortID: {
                 EXnelXlatePort_t *nelXlatePort = (EXnelXlatePort_t *)((void *)elementHeader + sizeof(elementHeader_t));

src/collector/collector.c
@@ -433,8 +433,8 @@ void FlushExporterStats(FlowSource_t *fs) {
         exporter_stats->stat[i].packets = e->packets;
         exporter_stats->stat[i].flows = e->flows;
 #ifdef DEVEL
-        printf("Stat: SysID: %u, version: %u, ID: %2u, Packets: %llu, Flows: %llu, Sequence Failures: %u, Padding Errors: %u\n", e->info.sysid,
-               e->info.version, e->info.id, e->packets, e->flows, e->sequence_failure, e->padding_errors);
+        printf("Stat: SysID: %u, version: %u, ID: %2u, Packets: %llu, Flows: %llu, Sequence Failures: %u\n", e->info.sysid, e->info.version,
+               e->info.id, e->packets, e->flows, e->sequence_failure);
 
 #endif
         // reset counters

src/output/output_raw.c
@@ -62,20 +62,40 @@ static void stringEXgenericFlow(FILE *stream, master_record_t *r) {
     char datestr1[64], datestr2[64], datestr3[64];
 
     struct tm *ts;
-    time_t when = r->msecFirst / 1000LL;
-    if (when == 0) {
-        strncpy(datestr1, "<unknown>", 63);
-    } else {
-        ts = localtime(&when);
-        strftime(datestr1, 63, "%Y-%m-%d %H:%M:%S", ts);
-    }
+    time_t when;
 
-    when = r->msecLast / 1000LL;
-    if (when == 0) {
-        strncpy(datestr2, "<unknown>", 63);
+    if (TestFlag(r->flags, V3_FLAG_EVENT)) {
+        uint64_t eventTime = r->msecEvent ? r->msecEvent : r->msecFirst;
+        when = eventTime / 1000LL;
+        if (when == 0) {
+            strncpy(datestr1, "<unknown>", 63);
+        } else {
+            ts = localtime(&when);
+            strftime(datestr1, 63, "%Y-%m-%d %H:%M:%S", ts);
+        }
+        fprintf(stream, "  Event time   =     %13llu [%s.%03llu]\n", (long long unsigned)eventTime, datestr1, eventTime % 1000LL);
+
     } else {
-        ts = localtime(&when);
-        strftime(datestr2, 63, "%Y-%m-%d %H:%M:%S", ts);
+        when = r->msecFirst / 1000LL;
+        if (when == 0) {
+            strncpy(datestr1, "<unknown>", 63);
+        } else {
+            ts = localtime(&when);
+            strftime(datestr1, 63, "%Y-%m-%d %H:%M:%S", ts);
+        }
+
+        when = r->msecLast / 1000LL;
+        if (when == 0) {
+            strncpy(datestr2, "<unknown>", 63);
+        } else {
+            ts = localtime(&when);
+            strftime(datestr2, 63, "%Y-%m-%d %H:%M:%S", ts);
+        }
+
+        fprintf(stream,
+                "  first        =     %13llu [%s.%03llu]\n"
+                "  last         =     %13llu [%s.%03llu]\n",
+                (long long unsigned)r->msecFirst, datestr1, r->msecFirst % 1000LL, (long long unsigned)r->msecLast, datestr2, r->msecLast % 1000LL);
     }
 
     if (r->msecReceived) {
@@ -88,12 +108,9 @@ static void stringEXgenericFlow(FILE *stream, master_record_t *r) {
     }
 
     fprintf(stream,
-            "  first        =     %13llu [%s.%03llu]\n"
-            "  last         =     %13llu [%s.%03llu]\n"
             "  received at  =     %13llu [%s.%03llu]\n"
             "  proto        =               %3u %s\n"
             "  tcp flags    =              0x%.2x %s\n",
-            (long long unsigned)r->msecFirst, datestr1, r->msecFirst % 1000LL, (long long unsigned)r->msecLast, datestr2, r->msecLast % 1000LL,
             (long long unsigned)r->msecReceived, datestr3, (long long unsigned)r->msecReceived % 1000L, r->proto, ProtoString(r->proto, 0),
             r->proto == IPPROTO_TCP ? r->tcp_flags : 0, FlagsString(r->proto == IPPROTO_TCP ? r->tcp_flags : 0));
 
@@ -497,21 +514,7 @@ static void stringsEXnselUserID(FILE *stream, master_record_t *r) {
 }  // End of stringsEXnselUserID
 
 static void stringsEXnelCommon(FILE *stream, master_record_t *r) {
-    char datestr[64];
-
-    time_t when = r->msecEvent / 1000LL;
-    if (when == 0) {
-        strncpy(datestr, "<unknown>", 63);
-    } else {
-        struct tm *ts = localtime(&when);
-        strftime(datestr, 63, "%Y-%m-%d %H:%M:%S", ts);
-    }
-
-    fprintf(stream,
-            "  nat event    =             %5u: %s\n"
-            "  Event time   =     %13llu [%s.%03llu]\n",
-            r->event, r->event_flag == FW_EVENT ? FwEventString(r->event) : EventString(r->event), (long long unsigned)r->msecEvent, datestr,
-            (long long unsigned)(r->msecEvent % 1000L));
+    fprintf(stream, "  nat event    =             %5u: %s\n", r->event, r->event_flag == FW_EVENT ? FwEventString(r->event) : EventString(r->event));
 
 }  // End of stringsEXnelCommon
 

src/test/nfgen.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2009-2022, Peter Haag
+ *  Copyright (c) 2009-2023, Peter Haag
  *  Copyright (c) 2004-2008, SWITCH - Teleinformatikdienste fuer Lehre und Forschung
  *  All rights reserved.
  *

commit aaaee7e8883d8e91ab7950602999ae388674f9c5

Don't clear flowStartMilliseconds when flowEndMilliseconds==0
This is a trivial bug introduced in
222419364892155de294792a262cca5e27d41993

src/inline/nffile_inline.c
@@ -470,7 +470,7 @@ static inline void ExpandRecord_v3(recordHeaderV3_t *v3Record, master_record_t *
 
 #ifdef NSEL
     if (output_record->msecFirst == 0) output_record->msecFirst = output_record->msecEvent;
-    if (output_record->msecLast == 0) output_record->msecFirst = output_record->msecEvent;
+    if (output_record->msecLast == 0) output_record->msecLast = output_record->msecEvent;
 #endif
 
 #ifdef DEVEL