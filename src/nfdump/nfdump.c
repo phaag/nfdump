109860b0b017c54cd2b2d2daf88d6ae3acaa96a6
Peter Haag <peter@people.ops-trust.net>
2024-06-02 16:42:42 +0200

    Remove debug output

--- src/nfdump/nfdump.c
+++ src/nfdump/nfdump.c
@@ -1248,7 +1248,6 @@ int main(int argc, char **argv) {
             }
             DisposeFile(nffile);
         } else {
-            printf("Mode: %u\n", outputParams->mode);
             PrintFlowTable(print_record, outputParams, GuessDir);
         }
     }
