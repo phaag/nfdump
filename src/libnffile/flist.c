0c86e8839e8863190c1d8f3e66a671f41e7d7c8d
Gijs Rijnders <gijs.rijnders@surf.nl>
2025-02-13 22:49:15 +0100

    Fixed bug where libnffile would not compile on FreeBSD

--- src/libnffile/flist.c
+++ src/libnffile/flist.c
@@ -235,7 +235,7 @@ static int CheckTimeWindow(char *filename, timeWindow_t *searchWindow);
 
 /* Functions */
 
-static int compare(const FTSENT **f1, const FTSENT **f2) { return strcmp((*f1)->fts_name, (*f2)->fts_name); }  // End of compare
+static int compare(const FTSENT * const *f1, const FTSENT *const *f2) { return strcmp((*f1)->fts_name, (*f2)->fts_name); }  // End of compare
 
 static void CleanPath(char *entry) {
     char *p, *q;
