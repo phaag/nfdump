54df3c692f9d103d482f153a2d40a25bdc789dab
Gijs Rijnders <gijs.rijnders@surf.nl>
2025-02-13 22:56:22 +0100

    The same syntax error resides in expire.c

--- src/collector/expire.c
+++ src/collector/expire.c
@@ -66,7 +66,7 @@ static uint32_t timeout = 0;
 
 static void PrepareDirLists(channel_t *channel);
 
-static int compare(const FTSENT **f1, const FTSENT **f2);
+static int compare(const FTSENT * const *f1, const FTSENT * const *f2);
 
 static void IntHandler(int signal) {
     switch (signal) {
@@ -251,7 +251,7 @@ uint64_t ParseTimeDef(char *s, uint64_t *value) {
 
 }  // End of ParseTimeDef
 
-static int compare(const FTSENT **f1, const FTSENT **f2) { return strcmp((*f1)->fts_name, (*f2)->fts_name); }  // End of compare
+static int compare(const FTSENT *const *f1, const FTSENT *const *f2) { return strcmp((*f1)->fts_name, (*f2)->fts_name); }  // End of compare
 
 void RescanDir(char *dir, dirstat_t *dirstat) {
     FTS *fts;
